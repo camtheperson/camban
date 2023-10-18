eb42f11e5c73ad6d0954bbd791e2f1a5400c07b2
Cameron Hermens <info@camtheperson.com>
2023-10-17 21:37:31 -0700

    feature: adds opengraph and twitter meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,15 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
     title: 'Camban',
     description: 'The easier way to do.',
+    openGraph: {
+        url: "https://www.camban.io",
+        title: "Camban",
+        description: "Camban is an agile project management tool for the masses.",
+        siteName: "Camban",
+        images: [{
+            url: "/camban-meta-bg.jpg",
+        }],
+    }
 }
 
 export default function RootLayout({
